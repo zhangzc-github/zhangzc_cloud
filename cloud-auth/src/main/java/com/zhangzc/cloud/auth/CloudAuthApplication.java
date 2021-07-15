bf93677374d380c8a2eb5e7379f433b1942f1c31
Zhichao Zhang <zhichao.zhang@stratifyd.cn>
2021-07-15 19:18:06 +0800

    init commit

---
+++ cloud-auth/src/main/java/com/zhangzc/cloud/auth/CloudAuthApplication.java
@@ -0,0 +1,13 @@
+package com.zhangzc.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAuthApplication.class, args);
+    }
+
+}
---
+++ cloud-auth/src/test/java/com/zhangzc/cloud/CloudAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangzc.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

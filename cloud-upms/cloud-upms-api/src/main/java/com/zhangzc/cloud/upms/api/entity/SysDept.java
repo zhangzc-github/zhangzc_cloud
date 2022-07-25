e671e4f0646a997be3e47d956f720747abc81c0c
Zhichao Zhang <zhichao.zhang@stratifyd.cn>
2022-07-25 16:38:30 +0800

    spring doc

--- cloud-upms/cloud-upms-api/src/main/java/com/zhangzc/cloud/upms/api/entity/SysDept.java
+++ cloud-upms/cloud-upms-api/src/main/java/com/zhangzc/cloud/upms/api/entity/SysDept.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -17,24 +18,28 @@ import java.time.LocalDateTime;
  * @author Zhichao Zhang
  * @date 2022/7/1 3:48 下午
  */
+@Schema(description = "部门")
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class SysDept extends Model<SysDept> {
 
 	private static final long serialVersionUID = 1L;
 
+	@Schema(description = "部门id")
 	@TableId(value = "dept_id", type = IdType.AUTO)
 	private Long deptId;
 
 	/**
 	 * 部门名称
 	 */
+	@Schema(description = "部门名称",required = true)
 	@NotBlank(message = "部门名称不能为空")
 	private String name;
 
 	/**
 	 * 排序
 	 */
+	@Schema(description = "排序值",required = true)
 	@NotNull(message = "排序值不能为空")
 	private Integer sortOrder;
 
